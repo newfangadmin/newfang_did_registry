1349a22d6411d0a97ed38c414e6caeea44500267
SauravKanchan <sauravnk30@gmail.com>
2019-12-03 13:51:38 +0530

    Resolve DID

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const EthrDID = require('ethr-did');
 const config = require('./config.json');
 const Web3 = require('web3');
 const ethers = require('ethers');
+const DIDResolver  = require('did-resolver');
+const EDR   = require('ethr-did-resolver');
 
 const currentProvider = new Web3.providers.HttpProvider(config.network);
 // const provider = new ethers.providers.Web3Provider(currentProvider);
@@ -11,4 +13,13 @@ const currentProvider = new Web3.providers.HttpProvider(config.network);
 let wallet = new ethers.Wallet(config.private_key);
 const ethrDid = new EthrDID({address: wallet.address, privateKey: wallet.privateKey, currentProvider, registry: config.EthereumDIDRegistry});
 
-console.log(ethrDid.did);
+console.log("DID: ",ethrDid.did);
+
+const providerConfig = { rpcUrl: config.network, registry: config.EthereumDIDRegistry }
+const ethrDidResolver = EDR.getResolver(providerConfig);
+const didResolver = new DIDResolver.Resolver(ethrDidResolver);
+
+(async () => {
+  console.log(await didResolver.resolve(ethrDid.did));
+
+})();
