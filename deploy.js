58e85a1da277d25184c3365802825ac6ea3d779c
SauravKanchan <sauravnk30@gmail.com>
2019-12-03 12:37:12 +0530

    Add compile and deploy script

---
+++ deploy.js
@@ -0,0 +1,44 @@
+const startTimestamp = Date.now();
+const ethers = require('ethers');
+const config = require('./config.json');
+const fs = require('fs-extra');
+const Web3 = require('web3');
+
+const currentProvider = new Web3.providers.HttpProvider(config.network);
+const provider = new ethers.providers.Web3Provider(currentProvider);
+
+const wallet = new ethers.Wallet(config.private_key, provider);
+console.log(`Loaded wallet ${wallet.address}`);
+
+async function deploy(contract_name) {
+  let compiled = require(`./build/${contract_name}.json`);
+
+  console.log(`\nDeploying ${contract_name} in ${config["network"]}...`);
+  let contract = new ethers.ContractFactory(
+    compiled.abi,
+    compiled.bytecode,
+    wallet
+  );
+  let instance = await contract.deploy();
+
+  console.log(`deployed at ${instance.address}`);
+  config[`${contract_name}`] = instance.address;
+  console.log("Waiting for the contract to get mined...");
+  await instance.deployed();
+  console.log("Contract deployed");
+  fs.outputJsonSync(
+    'config.json',
+    config,
+    {
+      spaces: 2,
+      EOL: "\n"
+    }
+  );
+
+}
+
+(async () => {
+  await deploy("EthereumDIDRegistry");
+})().catch(err => {
+  console.error(err);
+});
