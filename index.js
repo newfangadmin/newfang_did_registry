05b6eee3ee59cec3a8e2d8a15411eed97632a048
SauravKanchan <sauravnk30@gmail.com>
2019-12-04 16:26:59 +0530

    failed attempt of ethers

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ const DIDResolver = require('did-resolver');
 const EDR = require('ethr-did-resolver');
 const currentProvider = new Web3.providers.HttpProvider(config.network);
 const provider = new ethers.providers.Web3Provider(currentProvider);
-// const providerConfig = {rpcUrl: config.network, registry: config.EthereumDIDRegistry}
-// const ethrDidResolver = EDR.getResolver(providerConfig);
-// const didResolver = new DIDResolver.Resolver(ethrDidResolver);
-// var ethutil = require("ethereumjs-util");
-// var sha3 = require("js-sha3").keccak_256;
-// var BN = require("bn.js");
-
-var EC = require('elliptic').ec;
-
-// Create and initialize EC context
-// (better do it once and reuse it)
-var ec = new EC('secp256k1');
+const providerConfig = {rpcUrl: config.network, registry: config.EthereumDIDRegistry}
+const ethrDidResolver = EDR.getResolver(providerConfig);
+const didResolver = new DIDResolver.Resolver(ethrDidResolver);
+var ethutil = require("ethereumjs-util");
+var sha3 = require("js-sha3").keccak_256;
+var BN = require("bn.js");
 
 let didContract = new ethers.Contract(config.EthereumDIDRegistry, contractJson.abi, provider);
 
@@ -37,7 +31,7 @@ const attrTypes = {
 };
 
 let valid_days = 5;
-(async () => {
+// (async () => {
 //   console.log(`Add delegate, ${wallet1.address} is delegate of ${wallet.address}`);
 //   let tx = await didContract.connect(wallet).addDelegate(wallet.address, delegateTypes.Secp256k1SignatureAuthentication2018, wallet1.address, valid_days * 24 * 60 * 60);
 //   console.log("Transaction hash", tx.hash, "Waiting for transaction to be mined....");
@@ -57,20 +51,66 @@ let valid_days = 5;
 //   console.log(await didResolver.resolve(`did:ethr:${wallet.address}`));
 //
 //
+// })();
+function leftPad(data, size = 64) {
+  if (data.length === size) return data;
+  return "0".repeat(size - data.length) + data;
+}
 
-  let data = [
-    // "0x19",
-    // "0x00",
-    // config.EthereumDIDRegistry,
-    // parseInt(await didContract.functions.nonce(wallet.address)),
-    // wallet1.address,
-    // "addDelegate",
-    // ethers.utils.formatBytes32String("test"),
+function stripHexPrefix(str) {
+  if (str.startsWith("0x")) {
+    return str.slice(2);
+  }
+  return str;
+}
+
+async function signData(identity, signer, key, data) {
+  const nonce = await didContract.functions.nonce(signer);
+  const paddedNonce = leftPad(Buffer.from([nonce], 64).toString("hex"));
+  const dataToSign =
+    "1900" +
+    stripHexPrefix(config.EthereumDIDRegistry) +
+    paddedNonce +
+    stripHexPrefix(identity) +
+    data;
+  const hash = Buffer.from(sha3.buffer(Buffer.from(dataToSign, "hex")));
+  const signature = ethutil.ecsign(hash, key);
+  const publicKey = ethutil.ecrecover(
+    hash,
+    signature.v,
+    signature.r,
+    signature.s
+  );
+  return {
+    r: "0x" + signature.r.toString("hex"),
+    s: "0x" + signature.s.toString("hex"),
+    v: signature.v
+  };
+}
+
+(async () => {
+  let sig = await signData(
+    wallet.address,
+    wallet1.address,
+    Buffer.from(
+      wallet1.privateKey.substr(2,wallet1.privateKey.length),
+      "hex"
+    ),
+    Buffer.from("addDelegate").toString("hex") +
+    ethers.utils.formatBytes32String("attestor") +
+    stripHexPrefix(wallet2.address) +
+    leftPad(new BN(86400).toString(16))
+  );
+  let tx1 = await didContract.connect(wallet).functions.addDelegateSigned(
+    wallet.address,
+    sig.v,
+    sig.r,
+    sig.s,
+    ethers.utils.formatBytes32String("attestor"),
     wallet2.address,
-    "100"
-  ];
-
-  console.log(ec.sign(ethers.utils.keccak256(data), wallet.privateKey, "hex", {canonical: true}));
-
+    86400,
+  );
+  console.log(tx1.hash);
+  await tx1.wait();
+  console.log("transaction confirmed");
 })();
-
