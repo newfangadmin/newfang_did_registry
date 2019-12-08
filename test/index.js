2db230e418f052c494b09e1310ee28b8f4a98f1f
SauravKanchan <sauravnk30@gmail.com>
2019-12-08 22:17:02 +0530

    Add share file with test cases

--- test/index.js
+++ test/index.js
@@ -13,6 +13,11 @@ let IDs = [
   "0x3d725c5ee53025f027da36bea8d3af3b6a3e9d2d1542d47c162631de48e66c1c",
   "0x967f2a2c7f3d22f9278175c1e6aa39cf9171db91dceacd5ee0f37c2e507b5abe"
 ];
+let AccessTypes = {
+  read: ethers.utils.formatBytes32String("read"),
+  reshare: ethers.utils.formatBytes32String("reshare"),
+  delete: ethers.utils.formatBytes32String("delete")
+};
 
 describe('Ganache Setup', async () => {
   it('initiates ganache and generates a bunch of demo accounts', async () => {
@@ -50,5 +55,43 @@ describe('Contract initialization, DID creation', async () => {
       assert.ok(e.message.includes('revert'), e.message)
     }
   });
+
+});
+
+
+describe('Contract functions', async () => {
+  it('Share a file', async () => {
+    let tx = await newfangDID.functions.share(IDs[0], accounts[1], AccessTypes["read"],
+      ethers.utils.hashMessage("asdf"), 120);
+    await tx.wait();
+    let ACK = (await newfangDID.functions.accessSpecifier(IDs[0], AccessTypes["read"], accounts[1]));
+    assert.ok(ACK.encrypted_key !== "0x0000000000000000000000000000000000000000000000000000000000000000",
+      "encrypted key's hash not set");
+    assert.ok(parseInt(ACK.validity) !== 0, "Validity can not be 0")
+  });
+
+  it('share file with zero validity period', async () => {
+    try {
+      let tx = await newfangDID.functions.share(IDs[0], accounts[2], AccessTypes["read"],
+        ethers.utils.hashMessage("asdf"), 0);
+      await tx.wait();
+      assert(false, 'Should get an error');
+    } catch (e) {
+      assert.ok(e.message.includes('revert'), e.message)
+    }
+  });
+
+
+  it('share file with without owning the file', async () => {
+    try {
+      let tx = await newfangDID.connect(provider.getSigner(accounts[1])).functions.share(IDs[0], accounts[3], AccessTypes["read"],
+        ethers.utils.hashMessage("asdf"), 120);
+      await tx.wait();
+      assert(false, 'Should get an error');
+    } catch (e) {
+      assert.ok(e.message.includes('revert'), e.message)
+    }
+  });
+
 
 });
